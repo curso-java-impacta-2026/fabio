c2f0909ea27d73a1955d8bff6d5634d75bb5ebcb
FabioCCO80 <fabiocco1980@hotmail.com>
2026-09-05 11:19:14 -0300

    exemplo de utilização de Interfaces

---
+++ exemplos/src/br/com/impacta/cap10/Geladeira.java
@@ -0,0 +1,14 @@
+package br.com.impacta.cap10;
+
+public class Geladeira implements Eletrodomestico {
+
+    public void ligar() {
+        // implementação do método ligar declarado na interface Eletrodomestico
+    	System.out.println("Ligando Geladeira.");
+    }
+
+    public void desligar() {
+        // implementação do método desligar declarado na interface Eletrodomestico
+    	System.out.println("Desligando Geladeira.");
+    }
+}
