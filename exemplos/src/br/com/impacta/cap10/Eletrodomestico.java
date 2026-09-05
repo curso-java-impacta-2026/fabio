770a146c61566faa5d18cf81e9cf2ee4b7cdb35d
FabioCCO80 <fabiocco1980@hotmail.com>
2026-09-05 12:12:26 -0300

    instanciando um ojeto

--- exemplos/src/br/com/impacta/cap10/Eletrodomestico.java
+++ exemplos/src/br/com/impacta/cap10/Eletrodomestico.java
@@ -4,4 +4,24 @@ public interface Eletrodomestico {
 
     void ligar();
     void desligar();
+    
+    public final int VOLTAGEM = 110;
+    
+    default void acionarTimer(int minutos) {
+
+        /* Aguarda a quantidade de minutos informada. */
+        try { Thread.sleep(minutos * 6000); } catch (Exception e) {
+        	System.out.println("Deu erro na THREAD!!!");
+        }
+
+        /* Executa o método desligar conforme implementado na classe. */
+        desligar();
+    }
+    
+//    static void ligarTudo(Eletrodomestico... eletroArray) {
+//        for (Eletrodomestico eletro : eletroArray) {
+//            eletro.ligar();
+//        }
+    
 }
+
